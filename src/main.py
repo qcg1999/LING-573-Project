72ec40a9755c54a5e3f7fb16d9a8459f08dbcb11
John Dodson <jrdodson@patas.ling.washington.edu>
2019-04-19 13:02:37 -0700

    added textrank

--- src/main.py
+++ src/main.py
@@ -13,29 +13,19 @@ parser.add_argument("--aquaint2", type=str, default="/corpora/LDC/LDC08T25")
 parser.add_argument("--mode", type=str, choices=['train','dev','eval'],default='train')
 args, unks = parser.parse_known_args()
 
-loremipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
 def main():	
 	log_info("Finding document clusters for %s mode..." % args.mode)
-	'''topic_clusters is a dictionary of the form:
-		key: topic id
-		value: tuple(absolute file path, reference id)
-	
-		The reference id should be used to extract the correct XML block in the source file
-	'''
 	topic_clusters = find_topic_clusters(args.schema, (args.aquaint, args.aquaint2), args.mode)
 	log_info("Found %d document clusters." % len(topic_clusters))
 
-	# get list of feature vectors (for now, just plain text).
-	# feature_vectors should be an n by f 2d array where f is the number of features per topic.
-	# For now, f will be the number of documents and each feature will be the full text of 1 document.
-	for topic, docs in topic_clusters.iteritems():
+	log_info("Summarizing...")
+	for index,(topic, docs) in enumerate(topic_clusters.iteritems()):
+		if index != 0 and index % 10 == 0:
+			log_info("Processing cluster %d" % index)
 		feature_vectors = get_features(docs)
 	
 		sg = SummaryGenerator.SummaryGenerator();  #instantiate object
-		#words = nltk.corpus.brown.words(categories='news')
 		summary = sg.ToSummary(feature_vectors)   #generate summary
-#		log_info("EXAMPLE SUMMARY: %s " % summary)
 
 		id1 = topic[:-1]
                 id2 = topic[-1]
---
+++ src/textrank.py
@@ -0,0 +1,40 @@
+import numpy.linalg as la
+import numpy as np
+import networkx as nx
+
+
+def textrank(feature_matrix,src_matrix,vocab_size):
+	ranked = _textrank(feature_matrix,vocab_size)
+	ranked = sorted(((ranked[i],s) for i,s in enumerate(src_matrix)), reverse=True)
+	return ranked
+
+def _fast_pairwise_cosine_distance(matrix):
+	base_sims = np.dot(matrix,matrix.T).astype(np.float32)
+	inverse_square_magnitude = 1 / np.diag(base_sims)
+	inverse_square_magnitude[np.isinf(inverse_square_magnitude)] = 0
+	inverse_magnitude = np.sqrt(inverse_square_magnitude)
+	return np.array(base_sims * inverse_magnitude).T * inverse_magnitude
+
+def _textrank(feature_matrix, vocab_size):
+	similarities = _fast_pairwise_cosine_distance(feature_matrix)
+	return nx.pagerank(nx.from_numpy_array(similarities))
+
+
+if __name__ == "__main__":
+	import nltk
+	import pandas as pd
+	from sklearn.metrics import pairwise_distances
+	from sklearn.feature_extraction.text import CountVectorizer
+
+	corpus = [' '.join(s) for s in nltk.corpus.brown.sents(categories='news')][100:200]
+	vocab = set(nltk.corpus.brown.words(categories='news'))
+
+	vectorizer = CountVectorizer(vocabulary=vocab)
+	df = pd.DataFrame(data=corpus,columns=['sentences'])
+	feats = vectorizer.fit_transform(df['sentences'].values)
+
+	feats = np.array(feats.toarray(),dtype=np.int32)
+
+	ranked = textrank(feats, corpus, len(vocab))
+	for sent in ranked[0:5]:
+		print sent
