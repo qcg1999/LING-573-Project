7046d9b3e061c5b597e055a29472542c344d7a44
Charlie Guo <49220711+qcg1999@users.noreply.github.com>
2019-05-27 12:03:22 -0400

    Changed SCHEMA to point to only one cluster

--- src/run_short.sh
+++ src/run_short.sh
@@ -3,14 +3,14 @@
 if [ $# -ge 1 ]; then
 	SCHEMA=$1
 else
-	SCHEMA=./test/UpdateSumm09_test_topics_short.xml
+#	SCHEMA=./test/UpdateSumm09_test_topics_short.xml
+	SCHEMA=./test/UpdateSumm09_test_topics_D0934.xml
 fi
 
-# clean up summary dest folder
-for f in $(find ../outputs/D3/. -maxdepth 1 -type f -name '*' -print) 
-do 
-	rm -f $f 
-done
+#for f in $(find ../outputs/D4/. -maxdepth 1 -type f -name '*' -print) 
+#do 
+#	rm -f $f 
+#done
 
 # generate summary
 export PYTHON_CMD=python3
@@ -29,8 +29,8 @@ fi
 ### generate rouge evaluation
 export ROUGE_CMD=/dropbox/18-19/573/code/ROUGE/ROUGE-1.5.5.pl
 export ROUGE_DATA_DIR=/dropbox/18-19/573/code/ROUGE/data
-export ROUGE_CONFIG=../config/rouge_run_D3.xml
-export ROUGE_OUT=../results/D3_rouge_scores.out
+export ROUGE_CONFIG=../config/rouge_run_D4.xml
+export ROUGE_OUT=../results/D4_rouge_scores.out
 
 $ROUGE_CMD -e $ROUGE_DATA_DIR -a -n 2 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d $ROUGE_CONFIG > $ROUGE_OUT
 
