8bc8999eb8495486af5e117ea5e3b9fd41bd7f93
Charlie Guo <49220711+qcg1999@users.noreply.github.com>
2019-05-30 02:59:57 -0400

    Updated with more logic.

--- src/algorithms/compressor.py
+++ src/algorithms/compressor.py
@@ -9,9 +9,42 @@ parser_jar 		= '/NLP_TOOLS/parsers/stanford_parser/latest/stanford-parser.jar'
 
 os.environ['CLASSPATH'] = stanford_home
 
-def compress(sentences):
+def compress_list(sentences):
+	''' compress a list of sentences
+	'''
+	compressed = []
+	for s in sentences:
+		c = compress(s)
+		compressed.append(c)
 
-	return sentences
+	return compressed
+
+def compress(sentence):
+	''' compress a given sentence (as a string of words)
+		return a compressed sentence (also as a string of words)
+	'''	
+	print("sentence: ", sentence)
+
+	parser = stanford.StanfordParser (
+        model_path          = model_path,
+        path_to_models_jar  = parser_jar
+    )
+	#parse
+	s_parsed = parser.raw_parse(sentence)
+	#form a tree
+	for s in s_parsed:
+		print("parse: ", str(s))
+		tree1 = tree.Tree.fromstring(str(s))
+		break
+	
+	positions, position_flags = get_position_and_flags(tree1)
+	#for k, v in position_flags.items():
+	#	print("{1}: {0}".format(k, v))
+
+	compressed = realize(tree1, positions, position_flags)
+	#print ("compressed: ", compressed)
+
+	return compressed
 
 def traverse_tree(tree):
 	print("lable: ", tree.label())
@@ -35,23 +68,60 @@ def get_position_and_flags(tree):
 
 	# apply rules here
 	for p in positions:
-		if position_flags[p] == 0:
+		if position_flags[p] == 0:  #already visited and set false to keep
 			continue
 
-		if type(tree[p]) == nltk.tree.Tree:
+		if type(tree[p]) == nltk.tree.Tree:		# non-term node
 			#print ("label: ", tree1[p].label())
 			label = tree[p].label()
+
+			# rules:
 			# remove PP
 			if label == 'PP':
-				position_flags[p] = 0   #0: not to keep
+				#print("PP identified.")
+				#position_flags[p] = 0   #0: not to keep
 				#print("To be removed: \n", tree[p])
-				# mark child node
+				# mark 'flase' to keep on all child nodes
+				'''
 				for p2 in positions:
-					if is_leading(p, p2):
+					if is_leading(p, p2):  # is a child node
 						position_flags[p2] = 0	
 						#print("To be removed: \n", tree[p2])
+				'''
+				children = find_child_positions(p, positions)
+				#print("# children: ", len(children))
+				# mark to remove
+				if len(children) <= 15:
+					position_flags[p] = 0   #0: not to keep
+					#print("To be removed: \n", tree[p])
+					for c in children:
+						position_flags[c] = 0
+						#print("To be removed: \n", tree[c])
+
+			# other rules
+			elif label == 'DT':  # determiner 'the, The'
+				position_flags[p] = 0   #0: not to keep
+				children = find_child_positions(p, positions)
+				for c in children:
+					position_flags[c] = 0
+				
+
+		elif type(tree[p]) == str:	# term node
+			# rules on term node
+			continue
 
 	return positions, position_flags
+
+def find_child_positions(pos, positions):
+	''' find all child postions in 'positions' given postion 'pos'
+	'''
+	child_positions = []
+	for p in positions:
+		if is_leading(pos, p):
+			child_positions.append(p)
+
+	return child_positions
+
 
 def realize(tree, positions, position_flags):
 	leaf_list = []
@@ -85,81 +155,33 @@ def is_leading(t1, t2):
 if __name__ == "__main__":
 
 	# demonstration of usage
-
 	#s = '(ROOT (S (NP (NNP Europe)) (VP (VBZ is) (PP (IN in) (NP (DT the) (JJ same) (NNS trends)))) (. .)))'
-	#tree1 = tree.Tree.fromstring(s)
-	s_1 = 'I came to Richmond in 1998'
+	#s_1 = 'Europe is in the same trends'
+	s_1 = 'I live in Greater Richmond since 1998'
 	#s_1 = 'Senior Palestinian official Yasser Abed Rabbo denied on Tuesday reports saying that Palestinian leader Yasser Arafat has died in a French hospital'
 
-	print("sentence: ", s_1)
 
-	parser = stanford.StanfordParser (
-        model_path          = model_path,
-        path_to_models_jar  = parser_jar
-    )
-	#parse
-	s_parsed = parser.raw_parse(s_1)
-	#form a tree
-	for s in s_parsed:
-		#print("s: ", s)
-		print("s: ", str(s))
-		tree1 = tree.Tree.fromstring(str(s))
-		break
-	
-	#traverse_tree(tree1)
-
-	'''
-	# identify non-term nodes and term-node
-	positions = tree1.treepositions()
-	for p in positions:
-		print ("\n")	
-		print("p: ", p)
-		print("tree1[p]:", tree1[p])
-		print ("type: ",  type(tree1[p])) 
-		if type(tree1[p]) == nltk.tree.Tree:
-			print ("label: ", tree1[p].label())
-		elif type(tree1[p]) == str:
-			print ("leaf found: ",  tree1[p]) 
-	'''
-	
-	positions, position_flags = get_position_and_flags(tree1)
-	#print("position_flags: ", position_flags)
-	#print("position_flags:\n", ["{0}: {1}\n".format(k, v) for k, v in position_flags.items()])
-	#for k, v in position_flags.items():
-	#	print("{1}: {0}".format(k, v))
-
-	compressed = realize(tree1, positions, position_flags)
-	print ("compressed: ", compressed)
-
-	'''
-	t1 = (1, 2, 3)
-	t2 = (1, 2, 3, 4)
-	t3 = (2, 2, 3, 4)
-	lead = is_leading(t1, t2)
-	#print ("lead: ", lead) 
-	lead = is_leading(t1, t3)
-	#print ("lead: ", lead) 
-	'''
-
-	'''
-	sentences_list_01 = [
-		"It was a rainy day",
-		"Bob returned a book to the library", 
-		"The book was over due", 
-		"Bob was not happy", 
-	]
+	print("input sentence: ", s_1)
+	compressed_sent = compress(s_1)
+	print("compressed: ", compressed_sent)
 
 	sentences_list_02 = [
 		"Senior Palestinian official Yasser Abed Rabbo denied on Tuesday reports saying that Palestinian leader Yasser Arafat has died in a French hospital",
 		"As Arafat had struggled for life, there has been wild guess as to where he might be buried and where to hold the funeral service",
-		"Sunday night the French foreign minister, Michel Barnier, told LCI television that Arafat was alive but that his circumstances were complicated"
-		"Sunday night, the French foreign minister, Michel Barnier, told LCI television that Arafat was alive but that his circumstances were complicated"
+		"Sunday night, the French foreign minister, Michel Barnier, told LCI television that Arafat was alive but that his circumstances were complicated",
+		"Palestinian leader Yasser Arafat would be buried at his headquarters in the West Bank town of Ramallah, well-informed Palestinian sources said Tuesday"
 	]
-
-	compressed = compress(sentences_list_02);
-
-	print("compressed:\n", compressed)
-
 	'''
+	for s in sentences_list_02:
+		print("\n")
+		print("input sentence: ", s)
+		compressed = compress(s)
+		print("compressed:     ", compressed) 
+	'''
+
+	compressed_list_02 = compress_list(sentences_list_02)
+	for s in compressed_list_02:
+		print("\n")
+		print("compressed:     ", s)
 
 #eof
