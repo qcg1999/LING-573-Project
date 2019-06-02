add0a5c2c71c60292f4dc6402815e6d4288c3cf3
Charlie Guo <49220711+qcg1999@users.noreply.github.com>
2019-06-02 18:37:59 -0400

    Added for D4.

---
+++ src/D4.cmd
@@ -0,0 +1,10 @@
+executable = run.sh
+getenv = true
+output = stdout
+error = stderr
+log = log
+notification = never
+arguments = "/dropbox/18-19/573/Data/Documents/training/2009/UpdateSumm09_test_topics.xml"
+transfer_executable = true
+request_memory = 2*1024
+queue
